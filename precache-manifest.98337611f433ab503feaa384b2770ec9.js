bf424c732ea1a87af57ffe87768ef1a56649ffe3
thejhubbs <thejhubbs@gmail.com>
2019-08-03 12:18:36 -0400

    Updates

---
+++ precache-manifest.98337611f433ab503feaa384b2770ec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4429dcd9f0ad07a4496d4f82468f8c",
+    "url": "/app-my-top-nine/index.html"
+  },
+  {
+    "revision": "6c1d280ea29cb3913f38",
+    "url": "/app-my-top-nine/static/css/main.be859426.chunk.css"
+  },
+  {
+    "revision": "ac242ff4f15c4dc08ea5",
+    "url": "/app-my-top-nine/static/js/2.1455c33c.chunk.js"
+  },
+  {
+    "revision": "6c1d280ea29cb3913f38",
+    "url": "/app-my-top-nine/static/js/main.9db3c87f.chunk.js"
+  },
+  {
+    "revision": "ca82c140c22c43a0eaf2",
+    "url": "/app-my-top-nine/static/js/runtime~main.ec52237f.js"
+  }
+]);
